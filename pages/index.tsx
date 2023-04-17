160595eb686d875495c7df88eedd3ccd1771811f
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-04-17 17:58:55 +0530

    feat: re init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,105 +1,30 @@
-import {
-  GetUser,
-  PaperEmbeddedWalletSdk,
-  UserStatus,
-} from "@paperxyz/embedded-wallet-service-sdk";
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { useCallback, useEffect, useState } from "react";
-import { Login } from "../components/Login";
 import styles from "../styles/Home.module.css";
-import { Button } from "@chakra-ui/react";
-import { useAddress, useClaimNFT, useContract } from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
-  const [paper, setPaper] = useState<PaperEmbeddedWalletSdk>();
-  const [userDetails, setUserDetails] = useState<GetUser>();
-  const { contract: nftDrop } = useContract(
-    "0x01c20F77C2E24F88ee427f74111eDa74D154586A",
-    "nft-drop"
-  );
-  const { mutate: claimNft, isLoading, error } = useClaimNFT(nftDrop);
-  const address = useAddress();
-  console.log(error);
-
-  useEffect(() => {
-    const paper = new PaperEmbeddedWalletSdk({
-      clientId: "a70c4312-02d4-4de1-8979-9e6108ae8273",
-      chain: "Mumbai",
-    });
-    setPaper(paper);
-  }, []);
-
-  const fetchUserStatus = useCallback(async () => {
-    if (!paper) {
-      return;
-    }
-
-    const paperUser = await paper.getUser();
-    console.log("paperUser", paperUser);
-
-    setUserDetails(paperUser);
-  }, [paper]);
-
-  useEffect(() => {
-    if (paper && fetchUserStatus) {
-      fetchUserStatus();
-    }
-  }, [paper, fetchUserStatus]);
-
-  const logout = async () => {
-    const response = await paper?.auth.logout();
-    console.log("logout response", response);
-    await fetchUserStatus();
-  };
-
-  const callContractGasless = async () => {
-    // if (!userDetails) {
-    //   return;
-    // }
-
-    // const user =
-    //   userDetails.status === UserStatus.LOGGED_IN_WALLET_INITIALIZED
-    //     ? userDetails
-    //     : undefined;
-
-    // const params = {
-    //   contractAddress: "0xb2369209b4eb1e76a43fAd914B1d29f6508c8aae",
-    //   methodArgs: [user?.walletAddress ?? "", 1, 0],
-    //   methodInterface:
-    //     "function claimTo(address _to, uint256 _quantity, uint256 _tokenId) external",
-    // } as ContractCallInputType;
-    // console.log("params", params);
-    // try {
-    //   const result = await user?.wallet.gasless.callContract(params);
-    //   console.log("transactionHash", result?.transactionHash);
-    // } catch (e) {
-    //   console.error(`something went wrong sending gasless transaction ${e}`);
-    // }
-    const tx = claimNft({
-      to: address, // Use useAddress hook to get current wallet address
-      quantity: 1,
-    });
-
-    console.log(tx);
-  };
-
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        {userDetails && userDetails.status === UserStatus.LOGGED_OUT && (
-          <Login paper={paper} onLoginSuccess={fetchUserStatus} />
-        )}
-
-        {userDetails &&
-          userDetails.status === UserStatus.LOGGED_IN_WALLET_INITIALIZED && (
-            <div>
-              <h1>Logged in</h1>
-              <button onClick={logout}>Logout</button>
-              <Button onClick={callContractGasless} colorScheme="blue">
-                Call contract method (Gasless)
-              </Button>
-            </div>
-          )}
+        <ConnectWallet theme="dark" />
+        <Web3Button
+          contractAddress="0x01c20F77C2E24F88ee427f74111eDa74D154586A"
+          action={async (contract) => {
+            try {
+              const tx = await contract.erc721.claim(1);
+              console.log(tx);
+              alert("Claimed!");
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          theme="dark"
+          style={{
+            marginTop: "1rem",
+          }}
+        >
+          Claim
+        </Web3Button>
       </main>
     </div>
   );
